3f4392cad8dd9cf13c34d8c9cda1b18233cf99af
김홍규 <109813153+rredkyu@users.noreply.github.com>
2023-05-01 15:08:47 +0900

    git init
    init project

---
+++ src/main/java/com/kakaobank/placesearch/PlaceSearchApplication.java
@@ -0,0 +1,13 @@
+package com.kakaobank.placesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceSearchApplication.class, args);
+	}
+
+}
