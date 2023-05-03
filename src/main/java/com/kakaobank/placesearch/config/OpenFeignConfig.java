f00a9e050cc4672954d96146739abf1032fe8e05
redkyu <hongstar2311@gmail.com>
2023-05-03 23:11:34 +0900

    chore: feign package 추가로 인한 구조변경

--- src/main/java/com/kakaobank/placesearch/config/OpenFeignConfig.java
+++ src/main/java/com/kakaobank/placesearch/config/OpenFeignConfig.java
@@ -1,13 +1,12 @@
 package com.kakaobank.placesearch.config;
 
 import feign.Logger;
-import feign.codec.Decoder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients("com.kakaobank.placesearch.client")
+@EnableFeignClients("com.kakaobank.placesearch.feign.client")
 public class OpenFeignConfig {
 
     @Bean
--- src/main/java/com/kakaobank/placesearch/feign/client/KakaoLocalSearchFeignClient.java
+++ src/main/java/com/kakaobank/placesearch/feign/client/KakaoLocalSearchFeignClient.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @FeignClient(name = "KakaoLocalSearchFeignClient", url = "${kakao.local.url}")
 public interface KakaoLocalSearchFeignClient {
